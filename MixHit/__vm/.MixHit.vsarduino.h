2062f2186c7f4e83834e5d600f6b17afbed58460
lati1013 <43678503+lati1013@users.noreply.github.com>
2018-10-30 14:23:17 +0100

    Keine Ahnung was hier geändert wurde. Habe nur das Projekt in Visual Code geladen

--- MixHit/__vm/.MixHit.vsarduino.h
+++ MixHit/__vm/.MixHit.vsarduino.h
@@ -14,10 +14,11 @@
 #define _VSARDUINO_H_
 #define __ESP32_ESp32__
 #define __ESP32_ESP32__
+#define _VMDEBUG 1
 #define ESP_PLATFORM
 #define HAVE_CONFIG_H
 #define F_CPU 240000000L
-#define ARDUINO 10807
+#define ARDUINO 10806
 #define ARDUINO_ESP32_DEV
 #define ARDUINO_ARCH_ESP32
 #define ESP32
